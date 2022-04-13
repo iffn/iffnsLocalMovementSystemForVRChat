6635927ed1fa3f4cf9bbf65543d136fd7f688c83
iffn <iffn1990@gmail.com>
2022-04-13 16:10:57 +0200

    Added Skybox derotator

---
+++ Additionals/Skybox/SkyboxDerotator.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace iffnsStuff.iffnsVRCStuff.iffnsLocalMovementSystemForVRChat.Additionals
+{
+    public class SkyboxDerotator : UdonSharpBehaviour
+    {
+        public Transform ReferenceTransform;
+
+        private void Update()
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", -ReferenceTransform.rotation.eulerAngles.y);
+        }
+    }
+}
